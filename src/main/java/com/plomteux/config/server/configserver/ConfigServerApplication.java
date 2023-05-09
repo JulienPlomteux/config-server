f96aa984d51a6df1e81703fe94f56c2420726528
Mrlafontaine <archer19@hotmail.ca>
2023-05-08 23:30:09 -0400

    Continue...

--- src/main/java/com/plomteux/config/server/configserver/ConfigServerApplication.java
+++ src/main/java/com/plomteux/config/server/configserver/ConfigServerApplication.java
@@ -7,8 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-		
+		SpringApplication.run(ConfigServerApplication.class, args);	
 	}
 
 }
