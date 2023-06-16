12068f117e43ca811e71066d5a490f937b5fcd0e
Mrlafontaine <archer19@hotmail.ca>
2023-06-15 21:00:28 -0400

    Continue...

--- src/main/java/com/plomteux/config/server/configserver/configuration/SecurityConfiguration.java
+++ src/main/java/com/plomteux/config/server/configserver/configuration/SecurityConfiguration.java
@@ -1,26 +1,26 @@
-package com.plomteux.config.server.configserver.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.builders.WebSecurity.IgnoredRequestConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfiguration {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-            .httpBasic()
-                .and()
-            .csrf().disable();
-
-        return http.build();
-    }
-}
-
+//package com.plomteux.config.server.configserver.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity.IgnoredRequestConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfiguration {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//            .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//            .httpBasic()
+//                .and()
+//            .csrf().disable();
+//
+//        return http.build();
+//    }
+//}
+//
