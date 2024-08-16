2c92f1f1997dcd43019166e8a5ea7f0abf6e631b
Mrlafontaine <archer19@hotmail.ca>
2024-08-16 17:03:38 -0400

    cont

--- src/main/java/com/plomteux/config/server/configserver/configuration/SecurityConfiguration.java
+++ src/main/java/com/plomteux/config/server/configserver/configuration/SecurityConfiguration.java
@@ -3,22 +3,25 @@ package com.plomteux.config.server.configserver.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-            .httpBasic()
-                .and()
-            .csrf().disable();
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
 }
-
--- src/test/java/com/plomteux/config/server/configserver/ApplicationTests.java
+++ src/test/java/com/plomteux/config/server/configserver/ApplicationTests.java
@@ -3,9 +3,13 @@ package com.plomteux.config.server.configserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.plomteux.config.server.configserver.configuration.SecurityConfiguration;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ContextConfiguration(classes = SecurityConfiguration.class)
 class ApplicationTests {
 
 	@Test
